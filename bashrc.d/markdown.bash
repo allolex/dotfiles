024bceedfe8043fd0ea66a0bca9c66d442750dc2
Damon Davison <damon.davison@candidco.com>
2018-08-14 09:38:31 -0400

    Use Marko instead of Markoff for Markdown viewing

--- bashrc.d/markdown.bash
+++ bashrc.d/markdown.bash
@@ -1,5 +1,5 @@
 if [ "$HOSTNAME" = "nashville" ]; then
   export MARKDOWN_VIEWER_APP="/Applications/Marked.app"
 else
-  export MARKDOWN_VIEWER_APP="/Applications/Markoff.app"
+  export MARKDOWN_VIEWER_APP="/Applications/Marko.app"
 fi
