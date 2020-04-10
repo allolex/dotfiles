cb2826b9414283aec5dd81be63da7ef3516af9f2
Damon Davison <damon@curatur.com>
2020-04-10 11:44:30 -0400

    Fix up install script

--- install.bash
+++ install.bash
@@ -1,12 +1,22 @@
-cd $HOME
-ln -s $HOME/.dotfiles/asdfrc .asdfrc
-ln -s $HOME/.dotfiles/bashrc .bashrc
-ln -s $HOME/.dotfiles/bashrc.d .bashrc.d
-ln -s $HOME/.dotfiles/bin .bin
-ln -s $HOME/.dotfiles/config .config
-ln -s $HOME/.dotfiles/envrc .envrc
-ln -s $HOME/.dotfiles/gitconfig_nashville .gitconfig_nashville
-ln -s $HOME/.dotfiles/gitignore_global .gitignore_global
-ln -s $HOME/.dotfiles/inputrc .inputrc
-ln -s $HOME/.dotfiles/tool-versions .tool-versions
-ln -s $HOME/.dotfiles/vim .vim
+HOSTNAME=$(hostname)
+
+cd "$HOME" || echo "Could not cd to $HOME" && exit
+
+ln -s "$HOME/.dotfiles/asdfrc" .asdfrc
+ln -s "$HOME/.dotfiles/bashrc" .bashrc
+ln -s "$HOME/.dotfiles/bashrc.d" .bashrc.d
+ln -s "$HOME/.dotfiles/bin" .bin
+ln -s "$HOME/.dotfiles/config" .config
+ln -s "$HOME/.dotfiles/envrc" .envrc
+ln -s "$HOME/.dotfiles/gitignore_global" .gitignore_global
+ln -s "$HOME/.dotfiles/inputrc" .inputrc
+ln -s "$HOME/.dotfiles/tool-versions" .tool-versions
+ln -s "$HOME/.dotfiles/vim" .vim
+
+GITCONFIG_FOR_HOST="${HOME}/.dotfiles/gitconfig_${HOSTNAME}"
+if [[ -f "$GITCONFIG_FOR_HOST" ]]; then 
+  ln -s "$GITCONFIG_FOR_HOST" .gitconfig || echo "Could not link $GITCONFIG_FOR_HOST."
+else 
+  echo "$GITCONFIG_FOR_HOST does not exist. You will need it for git customization"
+  ls -1 ${HOME}/.dotfiles/gitconfig*
+fi
