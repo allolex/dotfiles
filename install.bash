e73b21dbf83a9d32e4bb2e2a09505ae6adc8edc1
Damon Davison <damon@curatur.com>
2020-04-10 11:22:51 -0400

    Add dotfiles install script

---
+++ install.bash
@@ -0,0 +1,12 @@
+cd $HOME
+ln -s $HOME/.dotfiles/asdfrc .asdfrc
+ln -s $HOME/.dotfiles/bashrc .bashrc
+ln -s $HOME/.dotfiles/bashrc.d .bashrc.d
+ln -s $HOME/.dotfiles/bin .bin
+ln -s $HOME/.dotfiles/config .config
+ln -s $HOME/.dotfiles/envrc .envrc
+ln -s $HOME/.dotfiles/gitconfig_nashville .gitconfig_nashville
+ln -s $HOME/.dotfiles/gitignore_global .gitignore_global
+ln -s $HOME/.dotfiles/inputrc .inputrc
+ln -s $HOME/.dotfiles/tool-versions .tool-versions
+ln -s $HOME/.dotfiles/vim .vim
