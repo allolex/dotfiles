3cc43cbbae41aaee447f05dbf8dadde09fef555b
Damon Davison <damon@curatur.com>
2020-04-17 15:03:53 -0400

    Ensure bash_profile loads bashrc

--- install.bash
+++ install.bash
@@ -4,6 +4,7 @@ cd "$HOME" || echo "Could not cd to $HOME" && exit
 
 ln -s "$HOME/.dotfiles/asdfrc" .asdfrc
 ln -s "$HOME/.dotfiles/bashrc" .bashrc
+ln -s "$HOME/.dotfiles/bash_profile" .bash_profile
 ln -s "$HOME/.dotfiles/bashrc.d" .bashrc.d
 ln -s "$HOME/.dotfiles/bin" .bin
 ln -s "$HOME/.dotfiles/config" .config
@@ -13,6 +14,7 @@ ln -s "$HOME/.dotfiles/inputrc" .inputrc
 ln -s "$HOME/.dotfiles/tool-versions" .tool-versions
 ln -s "$HOME/.dotfiles/vim" .vim
 
+# What about pric
 GITCONFIG_FOR_HOST="${HOME}/.dotfiles/gitconfig_${HOSTNAME}"
 if [[ -f "$GITCONFIG_FOR_HOST" ]]; then 
   ln -s "$GITCONFIG_FOR_HOST" .gitconfig || echo "Could not link $GITCONFIG_FOR_HOST."
